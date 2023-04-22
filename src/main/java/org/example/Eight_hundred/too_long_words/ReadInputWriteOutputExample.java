a846e4791076928320139b34eae1dbad330952f8
AlexRimC9 <alexrimc9@gmail.com>
2023-04-22 23:04:41 +0300

    Commit 1 Слишком длинные слова 22.04.2023

---
+++ src/main/java/org/example/Eight_hundred/too_long_words/ReadInputWriteOutputExample.java
@@ -0,0 +1,57 @@
+package org.example.Eight_hundred.too_long_words;
+
+//https://codeforces.com/problemset/problem/71/A
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ReadInputWriteOutputExample {
+
+    public static PrintWriter out;
+    public static class Reader {
+
+        // Класс Reader может быть расширен любыми статическими методами парсинга строки
+
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader.init(System.in);
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int amount = Reader.nextInt();
+
+        for(int i = 0; i < amount; i++){
+            String s = Reader.next();
+            out.println(TooLongWords.tooLongWords(s));
+        }
+        // Отправить накопленный вывод на stdout
+        out.close();
+    }
+}
---
+++ src/main/java/org/example/Eight_hundred/too_long_words/TooLongWords.java
@@ -0,0 +1,15 @@
+package org.example.Eight_hundred.too_long_words;
+
+public class TooLongWords {
+
+    public static String tooLongWords(String s) {
+        if (s.length() > 10) {
+            char start = s.charAt(0);
+            char end = s.charAt(s.length() - 1);
+            String length = Integer.toString(s.substring(1, s.length() - 1).length());
+            return start + length + end;
+        }
+        return s;
+    }
+
+}
